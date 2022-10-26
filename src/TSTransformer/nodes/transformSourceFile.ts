d2e895713828254ad9f795ba80b04b8374b935a4
Luke Chatton <chattonluke@gmail.com>
2022-10-26 12:03:41 -0400

    revert

--- src/TSTransformer/nodes/transformSourceFile.ts
+++ src/TSTransformer/nodes/transformSourceFile.ts
@@ -211,7 +211,7 @@ export function transformSourceFile(state: TransformState, node: ts.SourceFile) 
 	}
 
 	// add build information to the tree
-	luau.list.unshift(statements, luau.comment(`Compiled with unity-ts v${COMPILER_VERSION.split("-")[0]}`));
+	luau.list.unshift(statements, luau.comment(`Compiled with unity-ts v${COMPILER_VERSION.split("-")}`));
 
 	return statements;
 }
