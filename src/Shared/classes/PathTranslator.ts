e64e81abb27465c05dae70fb2b53cc284665dc61
Stephen Leitnick <sleitnick@gmail.com>
2023-04-14 09:45:41 -0400

    Fix paths

--- src/Shared/classes/PathTranslator.ts
+++ src/Shared/classes/PathTranslator.ts
@@ -44,12 +44,12 @@ export class PathTranslator {
 	public getOutputPath(filePath: string) {
 		const makeRelative = this.makeRelativeFactory();
 
-		if (filePath.includes(path.join("src/Shared"))) {
-			filePath = filePath.replace(path.join("src/Shared"), path.join("src/Shared/Resources/TS"));
-		} else if (filePath.includes(path.join("src/Server"))) {
-			filePath = filePath.replace(path.join("src/Server"), path.join("src/Server/Resources/TS"));
-		} else if (filePath.includes(path.join("src/Client"))) {
-			filePath = filePath.replace(path.join("src/Client"), path.join("src/Client/Resources/TS"));
+		if (filePath.includes("src/Shared")) {
+			filePath = filePath.replace("src/Shared", "src/Shared/Resources/TS");
+		} else if (filePath.includes("src/Server")) {
+			filePath = filePath.replace("src/Server", "src/Server/Resources/TS");
+		} else if (filePath.includes("src/Client")) {
+			filePath = filePath.replace("src/Client", "src/Client/Resources/TS");
 		}
 
 		const pathInfo = PathInfo.from(filePath);
