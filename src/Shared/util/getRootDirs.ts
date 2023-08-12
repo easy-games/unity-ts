6aeacd8f1e990afbe6f14544bb203bcf28137214
Luke Chatton <chattonluke@gmail.com>
2023-08-12 15:20:32 -0700

    Update getRootDirs.ts

--- src/Shared/util/getRootDirs.ts
+++ src/Shared/util/getRootDirs.ts
@@ -1,10 +1,12 @@
 import path from "path";
 import ts from "typescript";
 
-export function getRootDirs(compilerOptions: ts.CompilerOptions) {
-	// const rootDirs = compilerOptions.rootDir ? [compilerOptions.rootDir] : compilerOptions.rootDirs;
-	// assert(rootDirs);
-	// return rootDirs;
+export function getRootDirs(compilerOptions: ts.CompilerOptions): Array<string> {
+	if (compilerOptions.rootDir !== undefined) {
+		return [compilerOptions.rootDir];
+	} else if (compilerOptions.rootDirs !== undefined) {
+		return compilerOptions.rootDirs;
+	}
 	const rootDirs = [
 		path.join(compilerOptions.baseUrl!, "Server"),
 		path.join(compilerOptions.baseUrl!, "Client"),
