0acb7eea6fe0366e73dd3b2988b3a1ee92635397
Luke Chatton <chattonluke@gmail.com>
2023-08-13 11:23:59 -0700

    disable buildTypes

--- src/Project/functions/buildTypes.ts
+++ src/Project/functions/buildTypes.ts
@@ -1,7 +1,3 @@
-import fs from "fs-extra";
-import { getParsedCommandLine } from "Project/functions/getParsedCommandLine";
-import { LogService } from "Shared/classes/LogService";
-import { ProjectType } from "Shared/constants";
 import { ProjectData, ProjectOptions } from "Shared/types";
 import ts from "typescript";
 
@@ -16,32 +12,30 @@ function getTsConfigProjectOptions(tsConfigPath?: string): Partial<ProjectOption
 }
 
 export function buildTypes(data: ProjectData) {
-	if (data.projectOptions.type !== ProjectType.Game && data.projectOptions.type !== ProjectType.AirshipBundle) {
-		LogService.writeLine("Skipping types build.");
-		return;
-	}
-
-	LogService.writeLine("Building types...");
-
-	fs.removeSync(`../../../Types~/${process.env.npm_package_name}`);
-
-	// const projectOptions: ProjectOptions = Object.assign(
-	// 	{},
-	// 	DEFAULT_PROJECT_OPTIONS,
-	// 	getTsConfigProjectOptions(tsConfigPath),
-	// 	argv,
-	// );
-
-	const { fileNames, options } = getParsedCommandLine(data);
-	const typesProgram = ts.createProgram(fileNames, {
-		declaration: true,
-		declarationDir: `../../../Types~/${process.env.npm_package_name}`,
-		outDir: "temp",
-		rootDirs: ["src/Server", "src/Shared", "src/Server"],
-		plugins: [{ name: "typescript-transform-paths" }],
-	});
-	typesProgram.emit();
-	fs.removeSync("temp");
-	fs.createFileSync(`../../../Types~/${process.env.npm_package_name}/index.d.ts`);
-	LogService.writeLine("Finished building types!");
+	// if (data.projectOptions.type !== ProjectType.Game && data.projectOptions.type !== ProjectType.AirshipBundle) {
+	// 	LogService.writeLine("Skipping types build.");
+	// 	return;
+	// }
+	// LogService.writeLine("Building types...");
+	// fs.removeSync(`../../../Types~/${process.env.npm_package_name}`);
+	// // const projectOptions: ProjectOptions = Object.assign(
+	// // 	{},
+	// // 	DEFAULT_PROJECT_OPTIONS,
+	// // 	getTsConfigProjectOptions(tsConfigPath),
+	// // 	argv,
+	// // );
+	// const { fileNames, options } = getParsedCommandLine(data);
+	// const typesProgram = ts.createProgram(fileNames, {
+	// 	declaration: true,
+	// 	declarationDir: `../../../Types~/${process.env.npm_package_name}`,
+	// 	outDir: "temp",
+	// 	rootDirs: ["src/Server", "src/Shared", "src/Server"],
+	// 	plugins: [
+	// 		{ name: "typescript-transform-paths", afterDeclarations: true, transformProgram: true } as PluginImport,
+	// 	],
+	// });
+	// typesProgram.emit();
+	// fs.removeSync("temp");
+	// fs.createFileSync(`../../../Types~/${process.env.npm_package_name}/index.d.ts`);
+	// LogService.writeLine("Finished building types!");
 }
