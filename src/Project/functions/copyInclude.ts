fcd46857fa5b18ce0bc0af6ab76126225a55ed1e
Luke Chatton <chattonluke@gmail.com>
2023-08-14 10:39:04 -0700

    verbose copying

--- src/Project/functions/copyInclude.ts
+++ src/Project/functions/copyInclude.ts
@@ -1,6 +1,7 @@
 import copyfiles from "copyfiles";
 import fs from "fs-extra";
 import path from "path";
+import { LogService } from "Shared/classes/LogService";
 import { INCLUDE_PATH, ProjectType } from "Shared/constants";
 import { ProjectData } from "Shared/types";
 import { benchmarkIfVerbose } from "Shared/util/benchmark";
@@ -19,9 +20,22 @@ export function copyInclude(data: ProjectData) {
 
 export async function copyNodeModules(data: ProjectData) {
 	return new Promise<void>((resolve, reject) => {
+		LogService.writeLine("Copying node_modules...");
 		const nodeModules = path.join(data.projectPath, "..", "Bundles", "Shared", "Resources", "rbxts_include");
-		copyfiles(["node_modules/@easy-games/**/*.lua", nodeModules], err => {
-			resolve();
-		});
+		copyfiles(
+			["node_modules/@easy-games/**/*.lua", nodeModules],
+			{
+				soft: false,
+			},
+			err => {
+				if (err) {
+					LogService.writeLine("Failed to copy node_modules:");
+					console.error(err);
+				} else {
+					LogService.writeLine("Finished copying node_modules");
+				}
+				resolve();
+			},
+		);
 	});
 }
