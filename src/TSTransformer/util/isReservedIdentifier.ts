2ef9aad1e5f264d0b1a8aaa69e6f1bcbb1c7f6f3
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-03-05 10:18:56 +1300

    Cleaner globals override

--- src/TSTransformer/util/isReservedIdentifier.ts
+++ src/TSTransformer/util/isReservedIdentifier.ts
@@ -1,11 +1,7 @@
 import luau from "@roblox-ts/luau-ast";
 
-const globals = {
-	...luau.globals,
-	game: undefined,
-};
-
-delete globals.game; // IDK why LuauAST includes this
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { game, ...globals } = luau.globals;
 
 export function isReservedIdentifier(id: string) {
 	return Object.prototype.hasOwnProperty.call(globals, id);
