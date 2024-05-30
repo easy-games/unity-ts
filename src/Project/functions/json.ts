95493c77d40ce43794b3594d604d1580d0ab9541
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-05-30 15:58:26 +1200

    Fix diagnostic chains

--- src/Project/functions/json.ts
+++ src/Project/functions/json.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/ban-types
 
-import { ProjectData } from "Shared/types";
 import path from "path";
-import ts, { DiagnosticMessageChain } from "typescript";
+import { ProjectData } from "Shared/types";
+import ts, { DiagnosticMessageChain, LineAndCharacter } from "typescript";
 
 interface WatchReport {
 	messageText: string;
@@ -65,16 +65,54 @@ export function jsonReporter<K extends keyof RpcType>(request: K, value: RpcType
 			arguments: value,
 		}),
 	);
+
+	return value;
 }
 
-export function createJsonDiagnosticReporter(data: ProjectData): ts.DiagnosticReporter {
-	return diagnostic => {
-		const lineAndCol =
-			diagnostic.start !== undefined
-				? diagnostic.file?.getLineAndCharacterOfPosition(diagnostic.start)
-				: undefined;
+function jsonDiagnostic(rootPath: string, diagnostic: ts.Diagnostic, lineAndCol?: LineAndCharacter) {
+	if (typeof diagnostic.messageText === "object") {
+		const data = jsonReporter("fileDiagnostic", {
+			filePath: diagnostic.file ? path.relative(rootPath, diagnostic.file.fileName) : undefined,
+			message: diagnostic.messageText.messageText,
+			code: typeof diagnostic.code === "number" ? diagnostic.code : -1,
+			category: diagnostic.category,
+			position: diagnostic.start,
+			source: diagnostic.source,
+			line: lineAndCol?.line,
+			column: lineAndCol?.character,
+			length: diagnostic.length,
+			text:
+				diagnostic.start !== undefined && diagnostic.length !== undefined && diagnostic.file
+					? diagnostic.file.text.substring(diagnostic.start, diagnostic.start + diagnostic.length)
+					: undefined,
+		});
 
-		const rootPath = path.dirname(data.projectOptions.package);
+		const root = diagnostic.messageText;
+		if (root.next !== undefined) {
+			const reportNextChain = (chain: Array<ts.DiagnosticMessageChain>) => {
+				for (const item of chain) {
+					jsonReporter("fileDiagnostic", {
+						filePath: data.filePath,
+						message: item.messageText,
+						category: item.category,
+						position: undefined,
+						line: undefined,
+						length: undefined,
+						column: undefined,
+						code: item.code,
+						source: undefined,
+						text: undefined,
+					});
+
+					if (item.next) {
+						reportNextChain(item.next);
+					}
+				}
+			};
+
+			reportNextChain(root.next);
+		}
+	} else {
 		jsonReporter("fileDiagnostic", {
 			filePath: diagnostic.file ? path.relative(rootPath, diagnostic.file.fileName) : undefined,
 			message: diagnostic.messageText,
@@ -90,5 +128,17 @@ export function createJsonDiagnosticReporter(data: ProjectData): ts.DiagnosticRe
 					? diagnostic.file.text.substring(diagnostic.start, diagnostic.start + diagnostic.length)
 					: undefined,
 		});
+	}
+}
+
+export function createJsonDiagnosticReporter(data: ProjectData): ts.DiagnosticReporter {
+	return diagnostic => {
+		const lineAndCol =
+			diagnostic.start !== undefined
+				? diagnostic.file?.getLineAndCharacterOfPosition(diagnostic.start)
+				: undefined;
+
+		const rootPath = path.dirname(data.projectOptions.package);
+		jsonDiagnostic(rootPath, diagnostic, lineAndCol);
 	};
 }
